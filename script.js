284683a9a0178089ec2782e2b472b0624fff619d
Glitch (water-conservation) <none>
2019-02-07 01:26:17 +0000

    Original

---
+++ script.js
@@ -0,0 +1,64 @@
+function WA() {
+  var popup = document.getElementById("WA");
+  popup.classList.toggle("show");
+}
+function OR() {
+  var popup = document.getElementById("OR");
+  popup.classList.toggle("show");
+}
+function CA() {
+  var popup = document.getElementById("CA");
+  popup.classList.toggle("show");
+}
+function AK() {
+  var popup = document.getElementById("AK");
+  popup.classList.toggle("show");
+}
+function ID() {
+  var popup = document.getElementById("ID");
+  popup.classList.toggle("show");
+}
+function NV() {
+  var popup = document.getElementById("NV");
+  popup.classList.toggle("show");
+}
+function UT() {
+  var popup = document.getElementById("UT");
+  popup.classList.toggle("show");
+}
+function AZ() {
+  var popup = document.getElementById("AZ");
+  popup.classList.toggle("show");
+}
+function MT() {
+  var popup = document.getElementById("MT");
+  popup.classList.toggle("show");
+}
+function WY() {
+  var popup = document.getElementById("WY");
+  popup.classList.toggle("show");
+}
+function CO() {
+  var popup = document.getElementById("CO");
+  popup.classList.toggle("show");
+}
+function NM() {
+  var popup = document.getElementById("NM");
+  popup.classList.toggle("show");
+}
+function HI() {
+  var popup = document.getElementById("HI");
+  popup.classList.toggle("show");
+}
+function ND() {
+  var popup = document.getElementById("ND");
+  popup.classList.toggle("show");
+}
+function SD() {
+  var popup = document.getElementById("SD");
+  popup.classList.toggle("show");
+}
+function NE() {
+  var popup = document.getElementById("NE");
+  popup.classList.toggle("show");
+}
